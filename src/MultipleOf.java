6dfd6c01570d3eac66d9ef2b0d502acf3d96b327
GraphicStarr <mail2yonz@gmail.com>
2017-10-05 13:15:52 -0400

    Second commit

--- src/MultipleOf.java
+++ src/MultipleOf.java
@@ -5,8 +5,8 @@ public class MultipleOf {
     {
 
 
-        String mThree="Get it";
-        String mFive="Got it";
+        String mThree="Getit";
+        String mFive="Gotit";
         String mTen="Good";
 
 
@@ -15,25 +15,26 @@ public class MultipleOf {
 
             if(counter%3==0)
             {
-                System.out.print(mThree);
+                System.out.print(mThree+ " ");
             }
             if(counter%5==0)
             {
-                System.out.print(mFive);
+                System.out.print(mFive+ " ");
             }
 
            if(counter%10==0)
            {
-               System.out.print(mTen);
+               System.out.print(mTen+" ");
            }
 
 
-            if((counter %3!=0)&&(counter %5!=0)&&(counter%10!=0))
+           if((counter %3!=0)&&(counter %5!=0)&&(counter%10!=0))
             {
-                System.out.print(counter);
+                System.out.print(counter+ " ");
             }
 
-               System.out.println(" ");
+
+            System.out.println(" ");
 
            }
 
